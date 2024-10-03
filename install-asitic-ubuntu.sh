df4b69ff5e841b6e24e1a38e080a480d9d258b70
Iranildo Sales <68133032+Iranildot@users.noreply.github.com>
2024-10-03 18:47:32 -0300

    Update install-asitic-ubuntu.sh

--- install-asitic-ubuntu.sh
+++ install-asitic-ubuntu.sh
@@ -6,7 +6,7 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-cd /home/
+cd $HOME
 
 # CREATING A WORKSPACE FOLDER
 echo "Creating ASITIC folder"
@@ -51,42 +51,9 @@ echo "Prepering environment"
 echo "  Giving permition to asitic_linux file to be executed"
 chmod +x asitic_linux
 
-# INSTALLING EXPECT
-echo "  Installing expect"
-sudo apt -y update -qq
-sudo apt -y install expect -qq
+chmod -R 777 .
 
-# CREATING asitic EXECUTABLE FILE TO EXECUTE ASITIC FROM ANYWHERE
-echo "  Creating asitic executable file"
-echo -e "#!/usr/bin/expect -f\n\n\
-# CHECKS IF AN ARGUMENT WAS PASSED\n\
-if {[llength \$argv] != 1} {\n\
-    puts \"Usage: asitic <technology_name>.tek\"\n\
-    exit 1\n\
-}\n\
-\n\
-# CAPTURES THE NAME OF THE TECHNOLOGY FILE PASSED AS AN ARGUMENT\n\
-set tecnologia [lindex \$argv 0]\n\
-\n\
-# EXECUTES THE ASITIC\n\
-spawn /home/ASITIC/asitic_linux\n\
-\n\
-# AWAITS THE MESSAGE \"TECHNOLOGY FILE NAME?\"\n\
-expect \"Technology File Name?\"\n\
-\n\
-# SENDS THE NAME OF THE TECHNOLOGY FILE TO THE ASITIC\n\
-send \"/home/ASITIC/common/tek/\$tecnologia\r\"\n\
-\n\
-# ALLOWS INTERACTION WITH THE ASITIC AFTER SENDING THE FILE\n\
-interact" > asitic
-
-# GIVING PERMISSION FOR THE asitic FILE TO BE EXECUTED
-echo "  Giving permition to asitic file to be executed"
-chmod +x asitic
-
-sudo chmod -R 775 /home/ASITIC
-
-echo "  Adding asitic to PATH"
-mv ./asitic /usr/bin/
+echo "  Adding asitic_linux to PATH"
+mv ./asitic_linux /usr/bin/
 
 echo "Instalation done"
